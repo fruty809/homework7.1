77fa5b951ec5d69deb377c682811b41e4ecfebdb
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-21 19:17:01 +0600

    Update Magic.java

--- src/Magic.java
+++ src/Magic.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Magic {
+public class Magic extends Hero{
+    @Override
+    public void applySuperAbility() {
+        setTypeOfSuperAbility("Ice Stone");
+        System.out.println("Magic применил суперспособность " + getTypeOfSuperAbility());
+    }
 }
