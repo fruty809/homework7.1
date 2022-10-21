d04087f1a05b13881aad997196c9b99feffffc00
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-21 19:18:06 +0600

    Update Medic.java

--- src/Medic.java
+++ src/Medic.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Medic {
+public class Medic extends Hero{
+    @Override
+    public void applySuperAbility() {
+        setTypeOfSuperAbility("Heal all teammates");
+        System.out.println("Medic применил супер способность " + getTypeOfSuperAbility());
+    }
+
+    int healPoints;
+    public void increaseExperience(){
+        healPoints = healPoints + (healPoints/10);
+    }
+
+
 }
