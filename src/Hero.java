be6fff0d6ba800016093d1f2bdfffd9341cac960
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-21 19:16:45 +0600

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Hero {
+public abstract class Hero implements HavingSuperAbility {
+    private int age;
+    private int damage;
+    private String typeOfSuperAbility;
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setTypeOfSuperAbility(String typeOfSuperAbility) {
+        this.typeOfSuperAbility = typeOfSuperAbility;
+    }
+
+    public String getTypeOfSuperAbility() {
+        return typeOfSuperAbility;
+    }
 }
